83e32bad24d8350c3948742f101b5bf27bf377ec
kartik21-dev <kartik@utter.in>
2021-08-05 14:17:29 +0530

    feat: demo circular view , removed static methods

--- app/src/main/java/com/sling/jitpackdemo/MainActivity.java
+++ app/src/main/java/com/sling/jitpackdemo/MainActivity.java
@@ -2,32 +2,30 @@ package com.sling.jitpackdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
 import com.sling.circularviewsdk.*;
 
-public class MainActivity extends AppCompatActivity {
-    Button button;
+ public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
+       Button button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "ho raha hy", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, ""+button, Toast.LENGTH_SHORT).show();
-                ShowImage.img(MainActivity.this);
-            }
+            public void onClick(View view) { showimage(); }
         });
-
     }
 
-
+    private void showimage() {
+       Intent i = new Intent(getBaseContext(),MainActivity_sdk.class);
+       startActivity(i);
+    }
 
 }
--- circularviewsdk/src/main/java/com/sling/circularviewsdk/MainActivity_sdk.java
+++ circularviewsdk/src/main/java/com/sling/circularviewsdk/MainActivity_sdk.java
@@ -2,36 +2,36 @@ package com.sling.circularviewsdk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class MainActivity_sdk extends AppCompatActivity {
-    CircleImageView circleImageView;
-    private static MainActivity_sdk mainActivity;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sdk);
-        circleImageView = findViewById(R.id.profile_image1);
-
+        button = findViewById(R.id.bid);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callimage();
+            }
+        });
     }
 
-    public static MainActivity_sdk getInstance() {
-
-        if (mainActivity == null) {
-            mainActivity = new MainActivity_sdk();
-        }
-        return mainActivity;
+    public void callimage() {
+        String str = "https://source.unsplash.com/random";
+        CircleImageView circleImageView = findViewById(R.id.profile_image1);
+        Picasso.get().load(str).into(circleImageView);
     }
 
-
-    public void showimage(Context context) {
-        Toast.makeText(context, "" + circleImageView, Toast.LENGTH_SHORT).show();
-    }
 
 }
